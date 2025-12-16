7955b2abec7f004e25e0dbe05fac75ff948d6f50
Pengyu Chen <pychen96@gmail.com>
2025-12-16 21:25:24 +0000

    refactor

--- python/sglang/launch_server.py
+++ python/sglang/launch_server.py
@@ -29,8 +29,9 @@ def run_init_task(server_args):
 
 if __name__ == "__main__":
     server_args = prepare_server_args(sys.argv[1:])
-    init_proc = multiprocessing.Process(target=run_init_task, args=(server_args,))
-    init_proc.start()
+    if server_args.parallel_weight_downloading_model_path:
+        init_proc = multiprocessing.Process(target=run_init_task, args=(server_args,))
+        init_proc.start()
 
     try:
         run_server(server_args)
